dc265839fb9d0d30e58d6d6fad505b1ba173da8c
Nikita <evteev_n@inbox.ru>
2024-07-17 23:13:27 +0300

    some changes

--- flash-cards/index.js
+++ flash-cards/index.js
@@ -3,7 +3,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     // set onclick events to flash-cards
     const flash_cards = document.getElementsByClassName('flash_card')
-    flash_cards.addEventListener('click', function(event) {
-        console.log(event.target)
-    })
+    
+    for (let i = 0; i < flash_cards.length; i++) {
+
+        flash_cards[i].addEventListener('click',function(event) {
+            console.log('nothing is heppening ...')
+        })
+
+    }
 })
