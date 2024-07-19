116f4d2724b986e24610f61710952b9c7e822e8c
Nikita <evteev_n@inbox.ru>
2024-07-19 21:07:21 +0300

    implementrd card rotation

--- flash-cards/index.js
+++ flash-cards/index.js
@@ -1,14 +1,17 @@
 // https://www.codingnepalweb.com/best-javascript-games-for-beginners/
 
 document.addEventListener('DOMContentLoaded', function() {
-    // set onclick events to flash-cards
-    const flash_cards = document.getElementsByClassName('flash_card')
     
+    const flash_cards = document.getElementsByClassName('flash_card')
+
+    const isFlipedElements = []
+
     for (let i = 0; i < flash_cards.length; i++) {
 
-        flash_cards[i].addEventListener('click',function(event) {
-            console.log('nothing is heppening ...')
-        })
+        // set onclick events to flash-cards
+        flash_cards[i].onclick = flipCard
+
+        function flipCard(event) {}
 
     }
 })
