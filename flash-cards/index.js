7a9c56f5b9a5df6812b7f1d9ed5aa39e0da219b0
Nikita <evteev_n@inbox.ru>
2024-07-20 22:37:38 +0300

    changed logic in onclick event on the card

--- flash-cards/index.js
+++ flash-cards/index.js
@@ -3,15 +3,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     
     const flash_cards = document.getElementsByClassName('flash_card')
+    const flipedCards = []
 
-    const isFlipedElements = []
+    
 
     for (let i = 0; i < flash_cards.length; i++) {
 
         // set onclick events to flash-cards
-        flash_cards[i].onclick = flipCard
-
-        function flipCard(event) {}
-
+        flash_cards[i].onclick = onClick
     }
+
+    function onClick(event) {
+        flipCard(event)
+        isMatch(event)
+    }
+
+
+    function isMatch(event) {
+        event.currentTarget.onclick = null
+        flipedCards.push(event.currentTarget)
+    }
+
+    function flipCard(event) {
+        event.currentTarget.classList.toggle('is_fliped')
+    }
+
 })
