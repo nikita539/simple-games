d34a2cf53250f55a59a66180b139af8d9e63752a
Nikita <evteev_n@inbox.ru>
2024-07-22 20:54:08 +0300

    implemented function that flips matched cards

--- flash-cards/index.js
+++ flash-cards/index.js
@@ -1,31 +1,54 @@
 // https://www.codingnepalweb.com/best-javascript-games-for-beginners/
 
 document.addEventListener('DOMContentLoaded', function() {
-    
-    const flash_cards = document.getElementsByClassName('flash_card')
-    const flipedCards = []
+    const flashCards = document.getElementsByClassName('flash_card'); // list of all flash cards
 
-    
-
-    for (let i = 0; i < flash_cards.length; i++) {
+    for (let i = 0; i < flashCards.length; i++) {
 
         // set onclick events to flash-cards
-        flash_cards[i].onclick = onClick
+        flashCards[i].onclick = onClick
     }
-
-    function onClick(event) {
-        flipCard(event)
-        isMatch(event)
-    }
-
-
-    function isMatch(event) {
-        event.currentTarget.onclick = null
-        flipedCards.push(event.currentTarget)
-    }
-
-    function flipCard(event) {
-        event.currentTarget.classList.toggle('is_fliped')
-    }
-
 })
+
+//variables
+let flipedCards = [];
+let flipedCardsCount = 0
+
+// functions
+function onClick(event) {
+    flipCard(event)
+    isMatch(event)
+}
+
+function flipCard(event) {
+    event.currentTarget.classList.toggle('is_fliped')
+    flipedCards.push(event.currentTarget)
+}
+
+function isMatch() {
+    if (flipedCards.length == 2) {
+
+        if (flipedCards[0].dataset.number == flipedCards[1].dataset.number) {
+            console.log(flipedCards[0].dataset.number == flipedCards[1].dataset.number)
+
+            flipedCards.forEach((el) => {
+                el.onclick = null
+            })
+
+            flipedCardsCount++
+
+            flipedCards.length = 0
+        } else { 
+
+            setTimeout(function() {
+                flipedCards.forEach((el) => {
+                    el.classList.toggle('is_fliped')
+                })
+
+                flipedCards.length = 0
+            }, 2000)
+
+        }
+
+    }
+}
