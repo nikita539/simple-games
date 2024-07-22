96127184edc3dd266fa2628a04ad7734e5435a6a
Nikita <evteev_n@inbox.ru>
2024-07-22 23:14:57 +0300

    added function for handlers disabling

--- flash-cards/index.js
+++ flash-cards/index.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 //variables
+const card_container = document.getElementById('card_container');
 let flipedCards = [];
 let flipedCardsCount = 0
 
@@ -23,6 +24,11 @@ function onClick(event) {
 function flipCard(event) {
     event.currentTarget.classList.toggle('is_fliped')
     flipedCards.push(event.currentTarget)
+}
+
+function disableEvents(flag) {
+    if (flag) card_container.style.pointerEvents = 'none'  
+    else card_container.style.pointerEvents = ''  
 }
 
 function isMatch() {
@@ -39,11 +45,14 @@ function isMatch() {
 
             flipedCards.length = 0
         } else { 
+            disableEvents(true)
 
             setTimeout(function() {
                 flipedCards.forEach((el) => {
                     el.classList.toggle('is_fliped')
                 })
+
+                disableEvents(false)
 
                 flipedCards.length = 0
             }, 2000)
