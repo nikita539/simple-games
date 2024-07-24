5d2898a18350d91b8c75ed8dae06452f0dea4298
Nikita <evteev_n@inbox.ru>
2024-07-24 16:10:08 +0300

    changed logic of the app

--- flash-cards/index.js
+++ flash-cards/index.js
@@ -1,62 +1,68 @@
 // https://www.codingnepalweb.com/best-javascript-games-for-beginners/
 
-//variables
-const flashCards = document.getElementsByClassName('flash_card'); // list of all flash cards
-const card_container = document.getElementById('card_container');
-let flipedCards = [];
-let flipedCardsCount = 0
+const flashCards = document.getElementsByClassName('flash_card');
+const cardContainer = document.getElementById('card_container');
 
-for (let i = 0; i < flashCards.length; i++) {
+let firstCard, secondCard;
+let flipedCardCount = 0;
 
-    // set onclick events to flash-cards
-    flashCards[i].onclick = onClick
+const CLASS_NAME_TO_FLIP = 'is_fliped'
+
+
+for (let index = 0; index < flashCards.length; index++) {
+    flashCards[index].onclick = flipCard
 }
 
-// functions
-function onClick(event) {
-    flipCard(event)
-    isMatch(event)
-    gameIsOver()
-}
+
 function flipCard(event) {
-    event.currentTarget.classList.toggle('is_fliped')
-    flipedCards.push(event.currentTarget)
-}
 
-function isMatch() {
-    if (flipedCards.length == 2) {
+    if (firstCard) secondCard = event.currentTarget
+    else firstCard = event.currentTarget
+    
+    event.currentTarget.classList.toggle(CLASS_NAME_TO_FLIP)
+    
+    if (firstCard && secondCard) matchCards()
+} 
+function matchCards() {
+    disableField(true)
 
-        if (flipedCards[0].dataset.number == flipedCards[1].dataset.number) {
-            console.log(flipedCards[0].dataset.number == flipedCards[1].dataset.number)
+    if (firstCard.dataset.number == secondCard.dataset.number) {
+        firstCard.onclick = null
+        secondCard.onclick = null
 
-            flipedCardsCount++
+        firstCard = null
+        secondCard = null
 
-            flipedCards.length = 0
-        } else { 
-            disableEvents(true)
+        flipedCardCount++
 
-            setTimeout(function() {
-                flipedCards.forEach((el) => {
-                    el.classList.toggle('is_fliped')
-                })
-
-                disableEvents(false)
-
-                flipedCards.length = 0
-            }, 2000)
-
+        if (flipedCardCount == 8) {
+            gameIsOver()
         }
 
+        disableField(false)
+    } else {
+        setTimeout(() => {
+            firstCard.classList.remove(CLASS_NAME_TO_FLIP)
+            secondCard.classList.remove(CLASS_NAME_TO_FLIP)
+
+            firstCard = null
+            secondCard = null
+
+            disableField(false)
+        }, 2000)
     }
 }
-function disableEvents(flag) {
-    if (flag) card_container.style.pointerEvents = 'none'  
-    else card_container.style.pointerEvents = ''  
+function disableField(flag) {
+    if (flag) cardContainer.style.pointerEvents = 'none'
+    else cardContainer.style.pointerEvents = ''
 }
 function gameIsOver() {
-    if (flipedCardsCount == 8) {
-        setTimeout(() => {
-            alert("Game is over !")
-        }, 1000)
-    }
+    setTimeout(async () => {
+        alert('game is over !')
+
+        for (let index = 0; index < flashCards.length; index++) {
+            flashCards[index].classList.remove(CLASS_NAME_TO_FLIP)
+            flashCards[index].onclick = flipCard
+        }
+    }, 1000) 
 }
