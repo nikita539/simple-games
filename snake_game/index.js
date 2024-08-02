1f3098d1df6e97b0ed4dab15f330b3e0b6ecbe53
Nikita <evteev_n@inbox.ru>
2024-08-02 21:38:20 +0300

    created function to launch end stop the game

--- snake_game/index.js
+++ snake_game/index.js
@@ -1,18 +1,47 @@
 const snake = document.getElementById('snake')
 
-let interval = 1000;
+let int;
+let interval = 100;
 let xStep = 10;
 let yStep = 0;
 
-document.onclick = changeDirection
+document.onkeydown = (event) => {
 
-function changeDirection() {
-    xStep = xStep * -1
+    if (event.key == "Enter") {
+        if (int) {
+            clearInterval(int)
+            int = null
+            return
+        }
+
+        int = moving()
+    }
+
+    changeDirection(event)
+}
+
+function changeDirection(event) {
+    if (event.key == "ArrowUp") {
+        yStep = -10
+        xStep = 0
+    }
+    if (event.key == "ArrowDown") {
+        yStep = 10
+        xStep = 0
+    }
+    if (event.key == "ArrowLeft") {
+        xStep = -10
+        yStep = 0
+    }
+    if (event.key == "ArrowRight") {
+        xStep = 10
+        yStep = 0
+    }
 }
 
 function moving() {
 
-    setInterval(() => {
+    const inter = setInterval(() => {
 
         snake.style.left = `${snake.offsetLeft + xStep}px`
         snake.style.top = `${snake.offsetTop + yStep}px`
@@ -21,12 +50,42 @@ function moving() {
             snake.style.left = `${snake.offsetWidth * -1}px`
         }
 
-        if ( snake.offsetLeft == (snake.offsetWidth * -1) && xStep < 0) {
+        if ( snake.offsetLeft < (snake.offsetWidth * -1) && xStep < 0) {
             snake.style.left = `${window.innerWidth}px`
+        }
+
+        if (snake.offsetTop > window.innerHeight && yStep > 0) {
+            snake.style.top = `${snake.offsetHeight * -1}px`
+        }
+
+        if (snake.offsetTop < (snake.offsetHeight * -1) && yStep < 0 ) {
+            snake.style.top = `${window.innerHeight}px`
         }
 
     }, interval)
 
+    return inter
 }
 
-moving()
+
+function templater(template) { // <div>Hello {{someData}}</div>: String
+    let html = template
+
+    return function(data) { // {someData: "World !"}: Object<key<String>>
+        const regExp = /{{\w*}}/gi
+
+        while(html.match(regExp)) {
+            html = html.replace(html.match(regExp)[0], data)
+        }
+
+        return html
+    }
+}
+
+
+
+const tempalte = templater('<div>{{number}}{{number}}{{number}}</div>')
+
+const result = tempalte("World!")
+
+console.log(result)
